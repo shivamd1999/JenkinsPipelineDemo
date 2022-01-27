a92e2aa94dcc486eb0ef67d4a34be3c764d98aaf
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-27 14:53:51 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -5,5 +5,6 @@ now = time.time()
 
 for filename in os.listdir(path):
     filestamp = os.stat(os.path.join(path, filename)).st_mtime
-    filecompare = now - 7 * 86400if  filestamp < filecompare:
+    filecompare = now - 1 * 86400
+    if  filestamp < filecompare:
      print(filename)
