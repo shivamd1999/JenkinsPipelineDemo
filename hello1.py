9b4d6e58b81969fbc361662b5ef416adf1584055
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-27 18:30:10 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -8,7 +8,7 @@ os.chdir(path)
 
 for root,directories,files in os.walk(path,topdown=False): 
     for name in files:
-        t = os.stat(os.path.join(root, name))[1] 
+        t = os.stat(os.path.join(root, name))[18] 
         filetime = datetime.datetime.fromtimestamp(t) - today
 
 
