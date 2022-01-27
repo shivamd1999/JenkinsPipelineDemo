703a38b1d0c5b2ceafe7a3dbc2715f1c7c213711
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-27 14:52:46 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,4 +1,9 @@
-import os 
-for filenames in os.listdir(r'D:\projects'):
-  print('files:',filenames)
- 
+import os, time
+
+path = "D:\projects"
+now = time.time()
+
+for filename in os.listdir(path):
+    filestamp = os.stat(os.path.join(path, filename)).st_mtime
+    filecompare = now - 7 * 86400if  filestamp < filecompare:
+     print(filename)
