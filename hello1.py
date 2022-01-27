050a902a9b1f818fc0decf2374c4bdef3b81f470
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-27 15:09:31 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,10 +1,21 @@
-import os, time
+import os 
+import datetime
+import glob
+path = 'D:\projects'
 
-path = "D:\projects"
-now = time.time()
+today = datetime.datetime.today()#gets current time
+os.chdir(path) #changing path to current path(same as cd command)
 
-for filename in os.listdir(path):
-    filestamp = os.stat(os.path.join(path, filename)).st_mtime
-    filecompare = now - 15
-    if  filestamp < filecompare:
-     print(filename)
+#we are taking current folder, directory and files 
+#separetly using os.walk function
+for root,directories,files in os.walk(path,topdown=False): 
+    for name in files:
+        #this is the last modified time
+        t = os.stat(os.path.join(root, name))[8] 
+        filetime = datetime.datetime.fromtimestamp(t) - today
+
+        #checking if file is more than 7 days old 
+        #or not if yes then remove them
+        if filetime.days <= -80:
+            print(os.path.join(root, name), filetime.days)
+            os.remove(os.path.join(root, name))
