72b38ffb03041409592ed4ce8221024bbaa58c3b
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-27 22:45:35 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -8,11 +8,11 @@ os.chdir(path)
 
 for root,directories,files in os.walk(path,topdown=False): 
     for name in files:
-        t = os.stat(os.path.join(root, name))[0]
+        t = os.stat(os.path.join(root, name))[8]
         filetime = datetime.datetime.fromtimestamp(t) - today
 
 
-        if filetime.days <= 10:
+        if filetime.days <= -7:
             print(os.path.join(root, name), filetime.days)
             os.remove(os.path.join(root, name))
 
