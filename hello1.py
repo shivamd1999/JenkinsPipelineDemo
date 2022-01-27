7fc4db072e2e764064919eea42096146056ed984
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-27 17:56:08 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,17 +1,17 @@
 import os 
 import datetime
 import glob
-path = 'D:\projects'
+path = 'enter_path_here'
 
 today = datetime.datetime.today()
-os.chdir(path) 
-
+os.chdir(path)
 
 for root,directories,files in os.walk(path,topdown=False): 
     for name in files:
-        t = os.stat(os.path.join(root, name)) 
+        t = os.stat(os.path.join(root, name))[8] 
         filetime = datetime.datetime.fromtimestamp(t) - today
 
-        if filetime.time <= 67:
-            print(os.path.join(root, name), filetime.time)
+
+        if filetime.days <= 20:
+            print(os.path.join(root, name), filetime.days)
             os.remove(os.path.join(root, name))
