945b15c59ba378a9901bc56c5d8b6e3a3e08c8f4
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 14:49:14 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -15,52 +15,18 @@ for root,directories,files in os.walk(path,topdown=False):
             print(os.path.join(root, name), filetime.days)
             os.remove(os.path.join(root, name))
             
- 
-for dirpath,dirnames,filenames in os.walk(path):
-    print("current path",dirpath)
-    print("current directories",dirnames)
-    print("The Remaining files are:",filenames)
-    
-# importing required modules
-from zipfile import ZipFile
 import os
-
-def get_all_file_paths(directory):
-
-	# initializing empty file paths list
-	file_paths = 'D:\projects'
-
-	# crawling through directory and subdirectories
-	for root, directories, files in os.walk(directory):
-		for filename in files:
-			# join the two strings in order to form the full filepath.
-			filepath = os.path.join(root, filename)
-			file_paths.append(filepath)
-
-	# returning all file paths
-	return file_paths		
-
-def main():
-	# path to folder which needs to be zipped
-	directory = 'D:\projects'
-
-	# calling function to get all file paths in the directory
-	file_paths = get_all_file_paths(directory)
-
-	# printing the list of all files to be zipped
-	print('Following files will be zipped:')
-	for file_name in file_paths:
-		print(file_name)
-
-	# writing files to a zipfile
-	with ZipFile('D:\projects','w') as zip:
-		# writing each file one by one
-		for file in file_paths:
-			zip.write(file)
-
-	print('All files zipped successfully!')		
-
-
-if __name__ == "__main__":
-	main()
+import zipfile
+    
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), 
+                       os.path.relpath(os.path.join(root, file), 
+                                       os.path.join(path, 'D:\projects')))
+      
+zipf = zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED)
+zipdir('tmp/', zipf)
+zipf.close()
 
