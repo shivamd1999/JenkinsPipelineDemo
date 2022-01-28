49bc306f34fac1a1dd2258b126993bf00cc13a81
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 15:15:00 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -17,16 +17,14 @@ for root,directories,files in os.walk(path,topdown=False):
             
 import os
 import zipfile
-    
+
 def zipdir(path, ziph):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
         for file in files:
-            ziph.write(os.path.join(root, file), 
-                       os.path.relpath(os.path.join(root, file), 
-                                       os.path.join(path, 'D:\PythonDemo')))
-      
-zipf = zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED)
-zipdir('tmp/', zipf)
-zipf.close()
+            ziph.write(os.path.join(root, file))
 
+if __name__ == '__main__':
+    zipf = zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir('D:\PythonDemo', zipf)
+    zipf.close()
