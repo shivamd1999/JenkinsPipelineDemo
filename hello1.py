925528636a3e2090a2ec649dc1fd1b7d429e76b6
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 19:26:10 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,9 +1,6 @@
+import glob
 import os
-path = 'D:\fliker'
-os.chdir(path)
-files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
-oldest = files[0]
-newest = files[-1]
-print("Oldest:", oldest)
-print("Newest:", newest)
 
+list_of_files = glob.glob('D:\fliker')
+latest_file = max(list_of_files, key=os.path.getctime)
+print(latest_file)
