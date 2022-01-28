a2fd80974b4614fe1d6c6a6214329d1633a6260c
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 18:50:30 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -4,5 +4,4 @@ import os
 folder_path = "D:\fliker"
 files_path = os.path.join(folder_path, 'D:\fliker')
 files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True)
-print (files[0])
 print (files[0],files[1])
