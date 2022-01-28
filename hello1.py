8a2fb7d1a61372df1eb82544178f91dc314c293b
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 20:43:56 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -7,7 +7,6 @@ files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 newest = (files[-1],files[-2],files[-3],files[-4],files[-5])
 
 print("Newest:", newest)
-os.remove()
 
 import os
 from pathlib import Path
