e166237bb94f18f1ba8a17f14bd0727e399734c7
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 17:48:44 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -5,7 +5,7 @@ os.chdir(path)
 files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 
 oldest = file[0]
-newest = files[5]
+newest = files[-1]
 
 print "Oldest:", oldest
 print "Newest:", newest
