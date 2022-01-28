bc4fb8efa107c68efecc0d070090a5ae79653baf
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 14:14:41 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -21,3 +21,18 @@ for dirpath,dirnames,filenames in os.walk(path):
     print("current directories",dirnames)
     print("The Remaining files are:",filenames)
     
+from zipfile
+import ZipFile
+  
+# specifying the zip file name
+file_name = "D:\projects"
+  
+# opening the zip file in READ mode
+with ZipFile(file_name, 'r') as zip:
+    # printing all the contents of the zip file
+    zip.printdir()
+  
+    # extracting all the files
+    print('Extracting all the files now...')
+    zip.extractall()
+    print('Done!')
