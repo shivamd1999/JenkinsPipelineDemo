be2a5186682567cca65b4c0e470935f94ba94a3e
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 23:31:58 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -5,6 +5,7 @@ path = 'D:\jyjyjyj'
 os.chdir(path)
 files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 latest = (files[-1],files[-2],files[-3],files[-4],files[-5])
+os.remove(files)
 
 print("Latest:", latest)
 
