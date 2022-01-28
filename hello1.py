c66fa0d6665a28318a1c42b33447072230887762
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 16:09:42 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -14,9 +14,18 @@ for root,directories,files in os.walk(path,topdown=False):
         if filetime.days <= -18:
             print(os.path.join(root, name), filetime.days)
             os.remove(os.path.join(root, name))
-            
+
+import os
 import zipfile
-target = 'D:\hello'
-handle = zipfile.ZipFile('D:\hello','w')
-handle.write(target, compress_type = zipfile.ZIP_DEFLATED)
-handle.close()
+    
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), 
+                       os.path.relpath(os.path.join(root, file), 
+                                       os.path.join(path, 'D:\hello')))
+      
+zipf = zipfile.ZipFile('Python.zip', 'wb', zipfile.ZIP_DEFLATED)
+zipdir('tmp/', zipf)
+zipf.close()
