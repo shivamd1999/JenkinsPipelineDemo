b8eb001120d7c205f04c055e30a6d593e2c8e062
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 19:12:25 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,7 +1,25 @@
-import glob
-import os
+import os, glob, time
+root = 'D:\fliker'
+date_file_list = []
+for folder in glob.glob(root):
+    print "folder =", folder
+    
+    for file in glob.glob(folder + '/*.*'):
+       
+        stats = os.stat(file)
+        
+        lastmod_date = time.localtime(stats[8])
+       
+        date_file_tuple = lastmod_date, file
+        date_file_list.append(date_file_tuple)
 
-folder_path = "D:\fliker"
-files_path = os.path.join(folder_path, 'D:\fliker')
-files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True)
-print(files[0],files[1])
+
+date_file_list.sort()
+date_file_list.reverse() 
+print "%-40s %s" % ("filename:", "last modified:")
+for file in date_file_list:
+   
+    folder, file_name = os.path.split(file[1])
+   
+    file_date = time.strftime("%m/%d/%y %H:%M:%S", file[0])
+    print "%-40s %s" % (file_name, file_date)
