c534b805bcb5a1c9d3eba31fe7881a1e14847626
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 14:36:08 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -21,22 +21,4 @@ for dirpath,dirnames,filenames in os.walk(path):
     print("current directories",dirnames)
     print("The Remaining files are:",filenames)
     
-import os
-import shutil
-from zipfile import ZipFile
-from os import path
-from shutil import make_archive
-
-    # Check if file exists
-       if path.exists("D:\projects"):
-    # get the path to the file in the current directory
-        src = path.realpath("D:\projects");
-    # rename the original file
-        os.rename("career.guru99.txt","D:\projects")
-    # now put things into a ZIP archive
-        root_dir,tail = path.split(src)
-        shutil.make_archive("guru99 archive","zip",root_dir)
-    # more fine-grained control over ZIP files
-        with ZipFile("D:\projects", "w") as newzip:
-            newzip.write("D:\projects")
-            newzip.write("D:\projects")
+zipFilesInDir('sampleDir', 'sampleDir2.zip', lambda name : 'csv' in name)
