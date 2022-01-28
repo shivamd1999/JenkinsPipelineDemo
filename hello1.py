b5cc3e0633ef9bfbac95693aba9f8ca7bd6d071f
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 19:48:39 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,57 +1,21 @@
-# Import libraries
-
 import os
-
 import time
- 
-# Insert the directory path in here
-
 path = 'D:\fliker'
- 
-# Extracting all the contents in the directory corresponding to path
-
 l_files = os.listdir(path) 
- 
-# Iterating over all the files
 
 for file in l_files:
-
+ 
+ file_path = f'{path}\\{file}'
+ if os.path.isfile(file_path):
+  
+  try:
+   os.startfile(file_path, 'print')
+   print(f'Printing {file}')
+   time.sleep(5)
    
-
-  # Instantiating the path of the file
-
-    file_path = f'{path}\\{file}'
- 
-
-    # Checking whether the given file is a directory or not
-
-    if os.path.isfile(file_path):
-
-        try:
-
-            # Printing the file pertaining to file_path
-
-            os.startfile(file_path, 'print')
-
-            print(f'Printing {file}')
- 
-
-            # Sleeping the program for 5 seconds so as to account the 
-
-      # steady processing of the print operation.
-
-            time.sleep(5)
-
-        except:
-
-            # Catching if any error occurs and alerting the user
-
-            print(f'ALERT: {file} could not be printed! Please check\the associated softwares, or the file type.')
-
-    else:
-
-        print(f'ALERT: {file} is not a file, so can not be printed!')
-
-         
-
-print('Task finished!')
+   
+   except:
+    print(f'ALERT: {file} could not be printed! Please check\the associated softwares, or the file type.')
+  else:
+   print(f'ALERT: {file} is not a file, so can not be printed!')
+   print('Task finished!')
