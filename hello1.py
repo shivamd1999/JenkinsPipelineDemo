9d3bc81a24b0b21dc457f61002aa7f4015a972d0
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 17:55:06 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,15 +1,9 @@
-import os 
+import glob
+import os
 
-path = 'D:\fliker'
-os.chdir(path)
-files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
-
-Oldest = file[0]
-Newest = files[-1]
-
-print "Oldest:", oldest
-print "Newest:", newest
-print "All by modified oldest to newest:",files
+list_of_files = glob.glob('D:\fliker') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getctime)
+print(latest_file)
 
 import os
 from pathlib import Path
