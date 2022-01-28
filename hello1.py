2c35df75a696f8451f4a45cdb3c83002a351c508
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 16:52:42 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,36 +1,35 @@
-import os 
-import datetime
+# Import libraries
+import os
+import time
+
+# Insert the directory path in here
 path = 'D:\hello'
 
-today = datetime.datetime.today()
-os.chdir(path)
+# Extracting all the contents in the directory corresponding to path
+l_files = os.listdir(path)
 
-for root,directories,files in os.walk(path,topdown=False): 
-    for name in files:
-        t = os.stat(os.path.join(root, name))[8]
-        filetime = datetime.datetime.fromtimestamp(t) - today
+# Iterating over all the files
+for file in l_files:
 
+# Instantiating the path of the file
+	file_path = f'{path}\\{file}'
 
-        if filetime.days <= -18:
-            print(os.path.join(root, name), filetime.days)
-            os.remove(os.path.join(root, name))
-            
-for dirpath,dirnames,filenames in os.walk(path):
-    print("current path",dirpath)
-    print("current directories",dirnames)
-    print("The Remaining files are:",filenames)
+	# Checking whether the given file is a directory or not
+	if os.path.isfile(file_path):
+		try:
+			# Printing the file pertaining to file_path
+			os.startfile(file_path, 'print')
+			print(f'Printing {file}')
 
-import os
-import zipfile
-    
-def zipdir(path, ziph):
-    # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file), 
-                       os.path.relpath(os.path.join(root, file), 
-                                       os.path.join(path, 'D:\hello')))
-      
-zipf = zipfile.ZipFile('Python2.zip', 'w', zipfile.ZIP_DEFLATED)
-zipdir('tmp/', zipf)
-zipf.close()
+			# Sleeping the program for 5 seconds so as to account the
+	# steady processing of the print operation.
+			time.sleep(5)
+		except:
+			# Catching if any error occurs and alerting the user
+			print(f'ALERT: {file} could not be printed! Please check\
+			the associated softwares, or the file type.')
+	else:
+		print(f'ALERT: {file} is not a file, so can not be printed!')
+		
+print('Task finished!')
+
