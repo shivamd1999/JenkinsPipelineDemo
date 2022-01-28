bbe61e4e5bd295e61f8f312cab88f4ea4bb4d1eb
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-28 20:18:12 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -9,3 +9,18 @@ newest = (files[-1],files[-2],files[-3],files[-4],files[-5])
 print("Newest:", newest)
 print("all by modified older to newest:",files)
 
+import os
+from pathlib import Path
+from zipfile import ZipFile
+DOWNLOAD_DIR = Path("D:\hello")
+ZIPPED_FILE_DIR = Path("D:\hello")
+def get_list_of_all_folders(download_dir: Path):
+	return [f for f in download_dir.iterdir() if download_dir.is_dir()]
+def zip_files():
+	folder_list = get_list_of_all_folders(DOWNLOAD_DIR)
+	with ZipFile(ZIPPED_FILE_DIR / "my_zip.zip", "w") as zip:
+		for folder in folder_list:
+			zip.write(folder)
+
+zip_files()
+
