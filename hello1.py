e333053c1fb019d42cc620a68c79422513cb5bc4
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-01-29 15:53:01 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,24 +1,26 @@
 import os
 import time
-
-path = 'D:\jyjyjyj'
-os.chdir(path)
-files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
-latest = (files[-1],files[-2],files[-3],files[-4],files[-5])
-
-print("Latest:", latest)
-
-import os
 from pathlib import Path
 from zipfile import ZipFile
-DOWNLOAD_DIR = Path("D:\jyjyjyj")
-ZIPPED_FILE_DIR = Path("D:\jyjyjyj")
+path = 'D:\shivamdubey'
+os.chdir(path)
+files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+files.reverse()
+
+for file in files[5:]:
+    os.remove(os.path.join(path,file))
+
+latest = files[:5]
+print("Latest:", latest)
+
+ZIPPED_FILE_DIR = Path('D:\shivamdubey')
+
 def get_list_of_all_folders(download_dir: Path):
-	return [f for f in download_dir.iterdir() if download_dir.is_dir()]
+    return [f for f in download_dir.iterdir() if download_dir.is_dir()]
 def zip_files():
-	folder_list = get_list_of_all_folders(DOWNLOAD_DIR)
-	with ZipFile(ZIPPED_FILE_DIR / "my_zip.zip", "w") as zip:
-		for folder in folder_list:
-			zip.write(folder)
+    folder_list = get_list_of_all_folders(ZIPPED_FILE_DIR)
+    with ZipFile(ZIPPED_FILE_DIR / "Python.zip", "w") as zip:
+        for folder in folder_list:
+          zip.write(folder)
 
 zip_files()
