86b6c0120b056db67651b5250056b7a90baba949
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-02-01 17:30:30 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -2,23 +2,25 @@ import os
 import time
 import json
 #path='D:\Saved'
-
-data = json.loads('file.json')
+with open('file.json') as f:
+          data = json.loads(f)
+          data.close()
 print(data)
-os.chdir(path)
-files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
-files.reverse()
+# os.chdir(path)
+# files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+# files.reverse()
 
-for file in files[5:]:
-    os.remove(os.path.join(path,file))
+# for file in files[5:]:
+#     os.remove(os.path.join(path,file))
 
-latest = files[:5]
-print("Latest:", latest)
+# latest = files[:5]
+# print("Latest:", latest)
 
-import zipfile
-#working_folder = 'D:\Saved'
-files = os.listdir(working_folder)
-ZipFile = zipfile.ZipFile("Saved.zip", "w" )
-for a in files:
-    ZipFile.write(os.path.basename(a), compress_type=zipfile.ZIP_DEFLATED)
-ZipFile.close()
+# import zipfile
+# #working_folder = 'D:\Saved'
+# files = os.listdir(working_folder)
+# ZipFile = zipfile.ZipFile("Saved.zip", "w" )
+# for a in files:
+#     ZipFile.write(os.path.basename(a), compress_type=zipfile.ZIP_DEFLATED)
+# ZipFile.close()
+
