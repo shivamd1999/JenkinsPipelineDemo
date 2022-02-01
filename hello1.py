0ce5c744739010e9cd600d62f82526b93105d911
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-02-01 23:13:13 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,4 +1,25 @@
+import os
+import time
 import json
-with open('file.json','r') as persondata:
-          employee=json.load(persondata)
-print(employee)
+#path='D:\Saved'
+with open('file.json') as f:
+          data = json.load(f)
+          data.close()
+print(data)
+# os.chdir(path)
+# files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+# files.reverse()
+
+# for file in files[5:]:
+#     os.remove(os.path.join(path,file))
+
+# latest = files[:5]
+# print("Latest:", latest)
+
+# import zipfile
+# #working_folder = 'D:\Saved'
+# files = os.listdir(working_folder)
+# ZipFile = zipfile.ZipFile("Saved.zip", "w" )
+# for a in files:
+#     ZipFile.write(os.path.basename(a), compress_type=zipfile.ZIP_DEFLATED)
+# ZipFile.close()
