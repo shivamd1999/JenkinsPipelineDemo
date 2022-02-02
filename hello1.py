d38fca522a895e27a9f5fbd0bf7799fadcc3b77d
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-02-02 10:15:39 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,25 +1,43 @@
 import os
 import time
+import zipfile
 import json
-#path='D:\Saved'
-with open('file.json') as f:
-          data = json.load(f)
-          data.close()
-print(data)
-# os.chdir(path)
-# files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
-# files.reverse()
-
-# for file in files[5:]:
-#     os.remove(os.path.join(path,file))
-
-# latest = files[:5]
-# print("Latest:", latest)
-
-# import zipfile
-# #working_folder = 'D:\Saved'
-# files = os.listdir(working_folder)
-# ZipFile = zipfile.ZipFile("Saved.zip", "w" )
-# for a in files:
-#     ZipFile.write(os.path.basename(a), compress_type=zipfile.ZIP_DEFLATED)
-# ZipFile.close()
+try:
+          with open('file.json','r') as filesdata:
+                    filedata=json.load(filesdata)
+                    list_of_files = filter( lambda x: os.path.isfile(os.path.join(filedata['path'], x)),
+                                           os.listdir(filedata['path']) )
+                    list_of_files = sorted( list_of_files,
+                                           key = lambda x: os.path.getmtime(os.path.join(filedata['path'] x)))
+                    fileno=filedata['files']
+                    list_of_files.reverse()
+                    print("the latest files are:")
+                    for index, file in enumerate(list_of_files):
+                              if index > (fileno-1):
+                                        os.remove(os.path.join(filedata['path'],file))
+                              else:
+                                        file_path = os.path.join(filedata['path'],file))
+                                        timestamp_str = time.strftime('%m/%d/%Y :: %H:%M:%S'
+                                                                      time.gmtime(os.path.getmtime(file_path)))
+                                        print(timestamp_str, ' -->', file_name)
+                                        
+                     path=filedata['path']
+                     def prepare_zip(path):
+                                        new_file = filedata['path']+'.zip'
+                                        
+                                        zip = zipfile.ZipFile(new_file, 'w', zipfile.ZIP_DEFLATED)
+                                        
+                                        for dir_file, dir_name,files in os.walk(filedata['path']):
+                                                  f_path = dir_replace(dir_file, '')
+                                                  f_path = f_path and f_path +os.sep
+                                                  
+                                                  for file in files:
+                                                            zip.write(os.path.join(filedata['path'],file),f_path + file)
+                                                            zip.close()
+                                                            print("file zipped successfully")
+                                                            return os.rename(new_file,filedata['Zip_file_name'])
+                                                  prepare_zip(filedata['path'])
+                  
+except FileExistsError:
+          print(file Already Exist.")
+                                                                      
