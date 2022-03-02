c7356369e8d26311ad14273afa1a07d036771004
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-02 16:32:16 +0530

    Update Save.bat

--- Save.bat
+++ Save.bat
@@ -1,11 +1,10 @@
 
-pipeline {
-    agent any
+import os
+import time
+import subprocess
+programName = "notepad.exe"
+fileName = "C:\\Users\\shivamd1\\Documents\\Batch_Files\\save1.bat"
+p = subprocess.Popen([programName, fileName])
 
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello world'
-            }
-        }
-    }
+time.sleep(10)
+p.kill()
