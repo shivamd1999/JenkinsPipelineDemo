0db252b933a959e9a4cef51d8d2bedc28f3f8d0f
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-04 01:20:16 +0530

    Update Jenkinsemail.py

--- Jenkinsemail.py
+++ Jenkinsemail.py
@@ -1,14 +1,11 @@
 import smtplib
-from email.message import EmailMessage
+import SSL
 
-msg = EmailMessage()
-msg['Subject'] = 'test'
-msg['From'] = '7000766645az@gmail.com'
-msg['To'] = 'dubeyshivam264@gmail.com'
+server = smtplib.SMTP.SSL('smtp.gmail.com', 465)
 
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo()
 server.starttls()
 server.login('7000766645az@gmail.com','sxxbrfedfakhlreh')
-server.send_message(msg)
-server.quit()
+
+server.sendmail('7000766645az@gmail.com',
+                 'dubeyshivam264@gmail.com',
+                 "hello shivam")
