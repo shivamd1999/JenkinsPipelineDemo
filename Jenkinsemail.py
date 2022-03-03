7666cf7d0fe7631ccab31b69e729b48d8666a363
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-04 01:05:59 +0530

    Update Jenkinsemail.py

--- Jenkinsemail.py
+++ Jenkinsemail.py
@@ -1,10 +1,14 @@
 import smtplib
+from email.message import EmailMessage
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
+msg = EmailMessage()
+msg['Subject'] = 'test'
+msg['From'] = '7000766645az@gmail.com'
+msg['To'] = 'dubeyshivam264@gmail.com'
 
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
 server.starttls()
 server.login('7000766645az@gmail.com','sxxbrfedfakhlreh')
-
-server.sendmail('7000766645az@gmail.com',
-                 'dubeyshivam264@gmail.com',
-                 "hello shivam")
+server.send_message(msg)
+server.quit()
