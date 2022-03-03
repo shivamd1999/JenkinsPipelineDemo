b2d40ce820f080817536dd8710975bbd536a53df
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-03 14:07:52 +0530

    Update shivam.py

--- shivam.py
+++ shivam.py
@@ -1,9 +1,8 @@
 import os
 import time
 import subprocess
-programName = "notepad.exe"
 fileName = "C:\\Users\\shivamd1\\Documents\\Batch_Files\\save1.bat"
-p = subprocess.Popen([programName, fileName])
+p = subprocess.Popen([fileName])
 
 time.sleep(10)
 p.kill()
