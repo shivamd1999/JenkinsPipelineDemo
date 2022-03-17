f0b8464f5e83b7833fd4928ea6f55e7483eedeac
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-17 16:49:26 +0530

    Update htmlemail.py

--- htmlemail.py
+++ htmlemail.py
@@ -4,31 +4,63 @@ import smtplib, ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 ## The pandas library is only for generating the current date, which is not necessary for sending emails
-# import pandas as pd
+import pandas as pd
 
 # Define the HTML document
 html = '''
-   <!DOCTYPE html>
 <html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
-   <head>
-      <title>HTML Tables</title>
-   </head>
-	
-   <body>
-      <table border = "1">
-         <tr>
-            <td>ID 1, shivam</td>
-            <td>ID 2, jay</td>
-         </tr>
-         
-         <tr>
-            <td>ID 1, rohan</td>
-            <td>Id 2, harsh</td>
-         </tr>
-      </table>
-      
-   </body>
+td, th, td {
+  border: 1px solid black;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<h2>IDENTITY DETAILS</h2>
+
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Kpit</td>
+    <td>Shivam Dubey</td>
+    <td>India</td>
+  </tr>
+  <tr>
+    <td>Kpit</td>
+    <td>Jay</td>
+    <td>india</td>
+  </tr>
+  <tr>
+    <td>Kpit</td>
+    <td>Dharmendra</td>
+    <td>India</td>
+  </tr>
+  <tr>
+    <td>Kpit</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+</table>
+
+</body>
 </html>
     '''
 
@@ -38,13 +70,13 @@ password = 'oznxtbjcriaocrvj'
 email_to = 'dubeyshivam264@gmail.com'
 
 # Generate today's date to be included in the email Subject
-# date_str = pd.Timestamp.today().strftime('%Y-%m-%d')
+date_str = pd.Timestamp.today().strftime('%Y-%m-%d')
 
 # Create a MIMEMultipart class, and set up the From, To, Subject fields
 email_message = MIMEMultipart()
 email_message['From'] = email_from
 email_message['To'] = email_to
-# email_message['Subject'] = f'Report email - {date_str}'
+email_message['Subject'] = f'Report email - {date_str}'
 
 # Attach the html doc defined earlier, as a MIMEText html content type to the MIME message
 email_message.attach(MIMEText(html, "html"))
