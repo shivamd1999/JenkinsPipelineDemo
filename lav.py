d8c5b434767e4ce4410c97348e8a8151d09d9fab
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-23 21:06:01 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -1,10 +1,36 @@
-def buildnumber = currentBuild.number
-echo "build is ${currentBuild.number}"
-
-
-
-# def my_function(the):
-#     print(the + "")
-# my_function(" build number is ${env.BUILD_NUMBER}")
-# my_function("build number is ${env.JOB_NAME}")
-# my_function("build number is ${env.BUILD_URL}")
+pipeline{
+    agent any
+    
+    stages{
+        stage("Build number "){
+            steps{
+                echo "The build number is ${env.BUILD_NUMBER}"
+                echo "You can also use \${BUILD_NUMBER} -> ${BUILD_NUMBER}"                                                
+            }
+        }
+        stage("Jobs Name"){
+            steps{
+                echo "The build number is ${env.JOB_NAME}"
+                echo "You can also use \${JOB_NAME} -> ${JOB_NAME}"                                                
+            }
+        }
+        stage("Build URL"){
+            steps{
+                echo "The build number is ${env.BUILD_URL}"
+                echo "You can also use \${BUILD_URL} -> ${BUILD_URL}"                                                
+            }
+        }
+        stage("Build Duration"){
+            steps{
+                echo "The build number is Current time ${new Date()}"
+                echo "You can also use \${new Date()} -> ${new Date()}"                                                
+            }
+        }
+        stage("Build ok"){
+            steps{
+                echo "The build number is Current time ${currentBuild.durationString}"
+                echo "You can also use \${currentBuild.durationString} -> ${currentBuild.durationString}"                                                
+            }
+        }
+    }     
+}
