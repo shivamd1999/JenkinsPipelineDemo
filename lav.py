878fe8fe2b606203c60dda5a3189cf0cba6e4771
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-23 19:55:46 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -1,5 +1,10 @@
-def my_function(the):
-    print(the + "")
-my_function(" build number is ${env.BUILD_NUMBER}")
-my_function("build number is ${env.JOB_NAME}")
-my_function("build number is ${env.BUILD_URL}")
+import os 
+qualifier = os.environ("${env.BUILD_NUMBER}"}
+
+
+
+# def my_function(the):
+#     print(the + "")
+# my_function(" build number is ${env.BUILD_NUMBER}")
+# my_function("build number is ${env.JOB_NAME}")
+# my_function("build number is ${env.BUILD_URL}")
