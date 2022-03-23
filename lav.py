c095792943b308956084551624111ed3486ae2a1
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-23 15:02:12 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -2,5 +2,5 @@
     
 import jenkinsapi
 from jenkinsapi.jenkins import Jenkins
-server = Jenkins(jenkins_url,username='shivamd1',password='CzpW8Wy9')
+server = Jenkins(jenkins_url='http://localhost:8080/job/Task_1/',username='shivamd1',password='CzpW8Wy9')
 print(server.get_job("Task_1").get_last_buildnumber())
