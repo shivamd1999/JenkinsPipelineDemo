0155b6902a9ef098373df7c0869a70260e450eb0
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-23 14:38:20 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -1,1 +1,20 @@
-print("hwell")
+import jenkins
+import urllib2 
+import urllib
+import sys
+
+j = jenkins.Jenkins('http://localhost:8080')
+if not j.job_exists('Task_1'):
+    j.create_job('Task_1', jenkins.EMPTY_CONFIG_XML)
+
+j.reconfig_job('Task_1', jenkins.RECONFIG_XML)
+j.build_job('Task_1')
+
+last_build_number = j.get_job_info('Task_1')['lastCompletedBuild'] ['number']
+print last_build_number
+
+build_info=j.get_build_info('Task_1',last_build_number)
+if build_info['result']=='SUCCESS':
+    print "Build Success "
+else:
+    print " Build Failed "
