577bf1c59c09fd5addbfacb50ff13df21c567081
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-28 13:58:48 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -1,9 +1,10 @@
 import os
+import datetime
 print(os.environ['JOB_NAME'])
 print(os.environ['BUILD_NUMBER'])
 print(os.environ['BUILD_TAG'])
 print(os.environ['BUILD_URL'])
-print(os.environ['datetime.now()'])
+print(os.environ['datetime.datetime.now()'])
 
 
 
