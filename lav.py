6c23056529a7c0f6e22b6fbddf3676842d32dba0
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-28 12:59:54 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -1,36 +1,12 @@
-pipeline{
-    agent any
+def  function_name_print(a,b,c,d):
+    print(a,b,c,d)
     
-    stages{
-        stage("Build number "){
-            steps{
-                echo "The build number is ${env.BUILD_NUMBER}"
-                echo "You can also use \${BUILD_NUMBER} -> ${BUILD_NUMBER}"                                                
-            }
-        }
-        stage("Jobs Name"){
-            steps{
-                echo "The build number is ${env.JOB_NAME}"
-                echo "You can also use \${JOB_NAME} -> ${JOB_NAME}"                                                
-            }
-        }
-        stage("Build URL"){
-            steps{
-                echo "The build number is ${env.BUILD_URL}"
-                echo "You can also use \${BUILD_URL} -> ${BUILD_URL}"                                                
-            }
-        }
-        stage("Build Duration"){
-            steps{
-                echo "The build number is Current time ${new Date()}"
-                echo "You can also use \${new Date()} -> ${new Date()}"                                                
-            }
-        }
-        stage("Build ok"){
-            steps{
-                echo "The build number is Current time ${currentBuild.durationString}"
-                echo "You can also use \${currentBuild.durationString} -> ${currentBuild.durationString}"                                                
-            }
-        }
-    }     
-}
+function_name_print("${env.BUILD_NUMBER}","${env.JOB_NAME}","${env.BUILD_URL})
+
+
+
+
+
+
+
+
