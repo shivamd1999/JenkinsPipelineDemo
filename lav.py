d90d0720230f6edf700e12b15cf45337b2ebc216
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-28 13:03:05 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -1,5 +1,5 @@
-def  function_name_print(a,b,c,d):
-    print(a,b,c,d)
+def  function_name_print(a,b,c):
+    print(a,b,c)
     
 function_name_print("${env.BUILD_NUMBER}","${env.JOB_NAME}","${env.BUILD_URL})
 
