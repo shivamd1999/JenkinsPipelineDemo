53f4fe844ff655f10086dc746ad52b70c59b8858
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-28 13:25:46 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -2,7 +2,7 @@ def  funargs(*args):
     for item in args:
         print(item)
     
-har = ["${env.BUILD_NUMBER}","${env.JOB_NAME}","${env.BUILD_URL}"]
+har = [${env.BUILD_NUMBER}, ${env.JOB_NAME}, ${env.BUILD_URL}]
 funargs(*har)
 
 
