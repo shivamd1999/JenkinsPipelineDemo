29240cb6e2185761c7c0363dbe12569290a4e1cb
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-28 13:56:29 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -3,7 +3,7 @@ print(os.environ['JOB_NAME'])
 print(os.environ['BUILD_NUMBER'])
 print(os.environ['BUILD_TAG'])
 print(os.environ['BUILD_URL'])
-print(os.environ['currentBuild_durationString'])
+print(os.environ['datetime.now()'])
 
 
 
