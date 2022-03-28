11ec964145918335153a704d1216f0d0e37b0916
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-28 13:42:07 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -1,11 +1,15 @@
-def  funargs(*args):
-    for item in args:
-        print(item)
+import os
+print(os.environ['JOB_NAME'])
+
+
+
+
+# def  funargs(*args):
+#     for item in args:
+#         print(item)
     
-har = ["${currentBuild.currentResult}: Job ${JOB_NAME} build ${BUILD_NUMBER}\n ${currentBuild.currentResult}: Job ${JOB_NAME} \n ${currentBuild.currentResult}: ${BUILD_TAG}"]
-funargs(*har)
-
-
+# har = ["${currentBuild.currentResult}: Job ${JOB_NAME} build ${BUILD_NUMBER}\n ${currentBuild.currentResult}: Job ${JOB_NAME} \n ${currentBuild.currentResult}: ${BUILD_TAG}"]
+# funargs(*har)
 
 
 
