e97f5f399d83ec9fd2719e7d4b022d8e6fe1ac33
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-29 09:00:34 +0530

    Update lav.py

--- lav.py
+++ lav.py
@@ -3,52 +3,6 @@ import os
 print(os.environ['JOB_NAME'])
 print(os.environ['BUILD_NUMBER'])
 print(os.environ['BUILD_URL'])
-print(os.environ.get['BUILD_DURATION'])
 print(os.environ['BUILD_TAG'])
-
-# from datetime import datetime
-# now = datetime.now()
-# print (now.strftime("%Y-%m-%d %H:%M:%S"))
-
-# def  funargs(*args):
-#     for item in args:
-#         print(item)
-    
-# har = ["${currentBuild.currentResult}: Job ${JOB_NAME} build ${BUILD_NUMBER}\n ${currentBuild.currentResult}: Job ${JOB_NAME} \n ${currentBuild.currentResult}: ${BUILD_TAG}"]
-# funargs(*har)
-# pipeline{
-#     agent any
-    
-#     stages{
-#         stage("Build number "){
-#             steps{
-#                 echo "The build number is ${env.BUILD_NUMBER}"                                              
-#             }
-#         }
-#         stage("Jobs Name"){
-#             steps{
-#                 echo "The Jobs name is ${env.JOB_NAME}"                                              
-#             }
-#         }
-#         stage("Build URL"){
-#             steps{
-#                 echo "The Build URL is ${env.BUILD_URL}"                                               
-#             }
-#         }
-#         stage("Dates"){
-#             steps{
-#                 echo "The Updated date ${new Date()}"                                                
-#             }
-#         }
-#         stage("Build Duration"){
-#             steps{
-#                 echo "The Build Duration is  ${currentBuild.durationString}"                                               
-#             }
-#         }
-#     }     
-# }
-
-
-
 
 
