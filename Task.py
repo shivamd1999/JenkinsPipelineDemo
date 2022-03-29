7fbb20d88579b912d6e8a3f4067f6346dd06a66c
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-29 10:54:59 +0530

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,9 +1,10 @@
 import os
+import time
 
 print(os.environ['JOB_NAME'])
 print(os.environ['BUILD_NUMBER'])
 print(os.environ['BUILD_URL'])
 print(os.environ['BUILD_TAG'])
-print(os.environ['BUILD_DURATION'])
+print(os.environ['timestamp()'])
 
 
