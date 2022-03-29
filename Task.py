21d788a33e9ecf0e8d2e3e1a367293b3eb478646
shivamd1999 <98181118+shivamd1999@users.noreply.github.com>
2022-03-29 15:01:44 +0530

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,9 +1,9 @@
-# import os
+import os
 
-# print(os.environ['JOB_NAME'])
-# print(os.environ['BUILD_NUMBER'])
-# print(os.environ['BUILD_URL'])
-# print(os.environ['BUILD_TAG'])
+print(os.environ['JOB_NAME'])
+print(os.environ['BUILD_NUMBER'])
+print(os.environ['BUILD_URL'])
+print(os.environ['BUILD_TAG'])
 
 
 
